8cd3b5b804beed204f801b38f38aed75240fb82d
Deepak kumar <123898692+KDeepak5673@users.noreply.github.com>
2025-04-07 12:23:21 +0530

    Update newRequest.js

--- client/src/utils/newRequest.js
+++ client/src/utils/newRequest.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const newRequest = axios.create({
-  baseURL: "https://epics-farmflow-suhani.onrender.com",
+  baseURL: "https://epics-farmflow-suhani.onrender.com/api",
   withCredentials: true,
 });
 
